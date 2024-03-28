bad52d93ecbf38f7e16de38c6cabe36b8ace903b
Babayev Fuad <fuadtb@code.local>
2024-03-28 18:24:23 +0400

    Hometask

---
+++ Condition/script.js
@@ -0,0 +1,14 @@
+let userName = 'Fuad'
+let lastName = 'Babayev'
+let age = '16'
+let isAdmin = true
+
+if (age >= 20) {
+    console.log('1');
+}
+else if (userName == 'ulfat' || userName == 'Azerin' || isAdmin == true) {
+    console.log('2');
+}
+else {
+    console.log('3');
+}
